fbf4a17d34a2986c63289dc850b194e6967fbab5
d1v45 <128109634+d1v45@users.noreply.github.com>
2025-08-16 20:55:09 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -142,7 +142,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: config.googleAnalyticsID,
+        trackingId: 'UA-45666519-2',
       },
     },
   ],
